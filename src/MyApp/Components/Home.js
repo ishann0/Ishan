83e8a6bd5931e64fa3c90467c4367abfaef36adb
unknown <ishantstrixdevelopment@gmail.com>
2022-06-03 15:21:57 +0530

    New

---
+++ src/MyApp/Components/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Assests/CSS/Home.css'
+
+
+const Home = () => {
+  const width= '100px';
+  const width2= '150px'
+  return (
+    <div className='container'>
+        <h1>Home</h1>
+</div>
+  )
+}
+
+export default Home

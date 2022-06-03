28b1e0e7a3f6f26d09169894a9379eca66c93696
unknown <ishantstrixdevelopment@gmail.com>
2022-06-03 15:11:58 +0530

    new-changes

--- src/MyApp/Components/Contact.js
+++ src/MyApp/Components/Contact.js
@@ -1,14 +1,8 @@
 import React from 'react'
 import '../Assests/CSS/ContactCSS.css'
-import EmailLogo from '../Assests/Images/email.png'
-import PhoneLogo from '../Assests/Images/phone.png'
-import GitLogo from '../Assests/Images/github.png'
-import InstaLogo from '../Assests/Images/instagram.png'
-import { Row } from 'react-bootstrap';
-import { Col } from 'react-bootstrap';
+
 const contact = () => {
-const width = '80px';
-const widthh='80px'
+
   return (
     <>
 <h1>Contact</h1>
